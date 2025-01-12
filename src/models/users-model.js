14f7332b9f13dbb2c590c8c60901d85ab023ab92
juliohtm08 <jhtomoyuki345@gmail.com>
2025-01-12 13:05:29 -0300

    setup do projeto

---
+++ src/models/users-model.js
@@ -0,0 +1,28 @@
+const uuid = require('uuid').v4;
+const bcrypt = require('bcrypt');
+
+const users = [
+  { id: '1', name: 'Julio', email: 'julio@email.com', password: '123456' },
+  { id: '2', name: 'Jhon Doe', email: 'john@email.com', password: '123456' },
+];
+
+module.exports = {
+  getAllUsers: () => users,
+
+  getUserById: (id) => users.find((user) => user.id === id),
+
+  getUserByEmail: (email) => users.find((user) => user.email === email),
+
+  createUser: (name, email, password) => {
+    const newUser = {
+      id: uuid(),
+      name,
+      email,
+      password: bcrypt.hashSync(password, 10),
+    };
+
+    users.push(newUser);
+
+    return newUser;
+  },
+};
